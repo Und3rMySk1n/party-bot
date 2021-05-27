9e01a867e8332eee86f17d81c7e318c0a7845336
Aleksey Koptelin <AVKoptelin@vtbf.ru>
2021-05-27 12:30:48 +0300

    Added server start and requests listening.

--- bot.js
+++ bot.js
@@ -1,9 +1,31 @@
 const { Telegraf } = require('telegraf');
+const express = require('express');
 
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://alko-party-bot.herokuapp.com/';
+
+const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
+app.use(bot.webhookCallback(`/bot${BOT_TOKEN}`));
+
 bot.start((ctx) => ctx.reply('Hello from party bot! Lets get drunk!'));
 bot.launch();
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+/* Starting server */
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
+
+
