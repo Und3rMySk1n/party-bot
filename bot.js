0dd1a690f10dbb82dd4891c519c74731a7eb3ccf
Aleksey Koptelin <AVKoptelin@vtbf.ru>
2021-05-27 12:02:14 +0300

    Init bot behavior.

--- bot.js
+++ bot.js
@@ -1,1 +1,9 @@
-console.log('Hello from Party Bot!');
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Hello from party bot! Lets get drunk!'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
