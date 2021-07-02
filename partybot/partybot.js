d7626a1553a922ed1aab6f3a1d553ea480826f82
Aleksey Koptelin <AVKoptelin@vtbf.ru>
2021-07-02 21:33:41 +0300

    Fixed launch issues.

--- partybot/partybot.js
+++ partybot/partybot.js
@@ -11,12 +11,12 @@ module.exports = class PartyBot {
     }
 
     initCommands() {
-        this.bot.start(ctx => ctx.reply('Hello from party bot! Lets get drunk!', this.getMainMenu));
+        this.bot.start(ctx => ctx.reply('Hello from party bot! Lets get drunk!', this.getMainMenu()));
         this.bot.hears('Пользователи', ctx => ctx.reply('Тут будут пользователи'));
         this.bot.hears('Добавить напиток', ctx => ctx.reply('Тут меню добавления напитков'));
     }
     
-    private getMainMenu() {
+    getMainMenu() {
         return Markup.keyboard([
             ['Пользователи', 'Добавить напиток'],
         ]).resize().extra()
