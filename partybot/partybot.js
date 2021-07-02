650ba585806b363effb2e7ee11d80e47dee8403b
Aleksey Koptelin <AVKoptelin@vtbf.ru>
2021-07-02 21:58:44 +0300

    Added remove keyboard button.

--- partybot/partybot.js
+++ partybot/partybot.js
@@ -14,11 +14,16 @@ module.exports = class PartyBot {
         this.bot.start(ctx => ctx.reply('Hello from party bot! Lets get drunk!', this.getMainMenu()));
         this.bot.hears('Пользователи', ctx => ctx.reply('Тут будут пользователи'));
         this.bot.hears('Добавить напиток', ctx => ctx.reply('Тут меню добавления напитков'));
+        this.bot.hears('Хватит', ctx => ctx.reply('До встречи! Если что, ты знаешь, где меня найти: /start', this.removeKeyboard));
     }
     
     getMainMenu() {
-        return Markup.inlineKeyboard([
-            [{text: 'Пользователи'}, {text: 'Добавить напиток'}],
+        return Markup.keyboard([
+            ['Пользователи', 'Добавить напиток', 'Хватит'],
         ]).resize();
+    }
+    
+    removeKeyboard() {
+        return Markup.removeKeyboard(true);
     }
 }
